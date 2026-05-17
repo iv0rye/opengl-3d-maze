1015cdcdb90b93fff8e7e628548d73ac2b3664fa
Ivan Li <ivanli10252006@gmail.com>
2026-05-17 22:31:34 +1000

    Add shader class functions to header and generate def

---
+++ opengl-3d-maze/src/Shader.cpp
@@ -0,0 +1,37 @@
+#include "Shader.h"
+
+Shader::Shader(const char* vertexPath, const char* fragPath)
+{
+}
+
+void Shader::use()
+{
+}
+
+void Shader::setFloat(const std::string& name, float& value) const
+{
+}
+
+void Shader::setInt(const std::string& name, int& value) const
+{
+}
+
+void Shader::setVec1D(const std::string& name, glm::vec1& value) const
+{
+}
+
+void Shader::setVec2D(const std::string& name, glm::vec2& value) const
+{
+}
+
+void Shader::setVec3D(const std::string& name, glm::vec3& value) const
+{
+}
+
+void Shader::setVec4D(const std::string& name, glm::vec4& value) const
+{
+}
+
+void Shader::setMat4D(const std::string& name, glm::mat4& value) const
+{
+}
---
+++ opengl-3d-maze/src/Shader.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <string>
+#include <glm/glm.hpp>
+
+class Shader
+{
+public:
+	unsigned int ID;
+
+	Shader(const char* vertexPath, const char* fragPath);
+
+	void use();
+
+	// setters
+	void setFloat(const std::string& name, float& value) const;
+	void setInt(const std::string& name, int& value) const;
+
+	void setVec1D(const std::string& name, glm::vec1& value) const;
+	void setVec2D(const std::string& name, glm::vec2& value) const;
+	void setVec3D(const std::string& name, glm::vec3& value) const;
+	void setVec4D(const std::string& name, glm::vec4& value) const;
+
+	void setMat4D(const std::string& name, glm::mat4& value) const;
+};
+
