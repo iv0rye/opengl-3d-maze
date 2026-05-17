9aa2d6784e31d9cbcc761f0b320485493825900c
Ivan Li <ivanli10252006@gmail.com>
2026-05-17 22:52:01 +1000

    Implement check error logic and add into constructor logic

--- opengl-3d-maze/src/Shader.cpp
+++ opengl-3d-maze/src/Shader.cpp
@@ -1,22 +1,26 @@
 #include "Shader.h"
 #include <glad/glad.h>
+#include <iostream>
 
 Shader::Shader(const char* vertexSource, const char* fragSource)
 {
+	// TODO: implement geography shader source (havent learnt what that is)
+
+	// declare shader ids
 	unsigned int vertex;
 	unsigned int fragment;
-	int success;
-	char infoLog[512];
 
 	// generate vertex shader
 	vertex = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(vertex, 1, &vertexSource, NULL);
 	glCompileShader(vertex);
+	checkCompilationError(vertex, "VERTEX");
 
 	// generate fragment shader
 	fragment = glCreateShader(GL_FRAGMENT_SHADER);
 	glShaderSource(fragment, 1, &fragSource, NULL);
 	glCompileShader(fragment);
+	checkCompilationError(vertex, "FRAGMENT");
 
 	// generate shader
 	ID = glCreateProgram();
@@ -24,6 +28,7 @@ Shader::Shader(const char* vertexSource, const char* fragSource)
 	glAttachShader(ID, fragment);
 
 	glLinkProgram(ID);
+	checkCompilationError(vertex, "PROGRAM");
 
 	glDeleteShader(vertex);
 	glDeleteShader(fragment);
@@ -64,4 +69,29 @@ void Shader::setMat4D(const std::string& name, glm::mat4& value) const
 
 void Shader::checkCompilationError(unsigned int shader, std::string type)
 {
+	int success;
+	char infoLog[512];
+
+	if (type != "PROGRAM")
+	{	
+		// if getting shader successful, no compile-time error and return early, otherwise print error
+		glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+
+		if (success)
+			return;
+
+		glGetShaderInfoLog(shader, 512, NULL, infoLog);
+		std::cout << "error: compilation error on type: " + type + "\n" + infoLog << std::endl;
+	}
+	else
+	{
+		// if getting program successful, no link-time error and return early, otherwise print error
+		glGetProgramiv(shader, GL_LINK_STATUS, &success);
+
+		if (success)
+			return;
+
+		glGetProgramInfoLog(shader, 512, NULL, infoLog);
+		std::cout << "error: link-time error on type: " + type + "\n" + infoLog << std::endl;
+	}
 }
