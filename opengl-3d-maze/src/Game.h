90c061d72fbef3b5dc36c72a4cb2fc5d74cff7fa
Ivan Li <ivanli10252006@gmail.com>
2026-05-17 22:10:52 +1000

    Add Game class to store game related functionality

---
+++ opengl-3d-maze/src/Game.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+enum GameState
+{
+	GAME_ACTIVE,
+	GAME_MENU
+};
+
+class Game
+{
+public:
+	unsigned int Width, Height;
+
+	Game(unsigned int width, unsigned int height);
+	~Game();
+
+	// initialise game -> load obj
+	void Init();
+
+	GameState State;
+
+	// arr to process input
+	bool Keys[1024];
+
+	// game loop
+	void ProcessInput(float dt);
+	void Update(float dt);
+	void Render();
+};
+
