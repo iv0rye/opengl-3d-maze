2a23d172ea95c0bf65aa4a70ad8400e174abc272
Ivan Li <ivanli10252006@gmail.com>
2026-05-24 00:35:36 +1000

    Add method for loading shader into resource manager

--- opengl-3d-maze/src/ResourceManager.cpp
+++ opengl-3d-maze/src/ResourceManager.cpp
@@ -1,24 +1,66 @@
 #include "ResourceManager.h"
 #include "stb_image/stb_image.h"
+#include <fstream>
+#include <sstream>
 #include <iostream>
 
 ResourceManager::ResourceManager() {}
 
 Shader ResourceManager::SetShader(const char* vShaderFile, const char* fShaderFile, std::string shaderName)
 {
-    return Shader();
+    // read file
+    std::string vertexCode;
+    std::string fragmentCode;
+
+    std::ifstream vertexShaderFile;
+    std::ifstream fragmentShaderFile;
+
+    vertexShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+    fragmentShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+
+    try
+    {
+        // open and read into string streams
+        vertexShaderFile.open(vShaderFile);
+        fragmentShaderFile.open(fShaderFile);
+
+        std::stringstream vShaderStream;
+        std::stringstream fShaderStream;
+
+        vShaderStream << vertexShaderFile.rdbuf();
+        fShaderStream << fragmentShaderFile.rdbuf();
+
+        vertexShaderFile.close();
+        fragmentShaderFile.close();
+
+        // string streams -> strings
+        vertexCode = vShaderStream.str();  
+        fragmentCode = fShaderStream.str(); 
+    }
+    catch (std::ifstream::failure& e)
+    {
+        std::cout << "ERROR: Shader file unsucessfully read" << std::endl;
+    }
+
+    // generate shader with c string (array of char)
+    Shaders[shaderName] = Shader(vertexCode.c_str(), fragmentCode.c_str());
+
+    return Shaders[shaderName];
 }
 
 Texture ResourceManager::SetTexture(const char* textureFile, std::string textureName)
 {
     stbi_set_flip_vertically_on_load(true);
 
+    // create texture data
     int width, height, nrChannels;
     unsigned char* textureData = stbi_load(textureFile, &width, &height, &nrChannels, 0);
-
+    
+    // error handling for if stbi doesnt load texture file
     if (!textureData)
         throw std::runtime_error("ERROR: Failed to load texture: " + textureName);
-       
+     
+    // set texture, free memory and return texture
     Textures[textureName] = Texture(textureData, width, height);
 
     stbi_image_free(textureData);
