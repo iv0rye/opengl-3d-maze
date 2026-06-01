23fe4ac5b96247ccf9ece76071206b8644e2aed6
Ivan Li <ivanli10252006@gmail.com>
2026-06-01 19:45:09 +1000

    Improve renderer init function

--- opengl-3d-maze/src/ObjectRenderer.cpp
+++ opengl-3d-maze/src/ObjectRenderer.cpp
@@ -77,4 +77,6 @@ void ObjectRenderer::initRenderData()
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
     glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
 
+    glBindVertexArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
 }
