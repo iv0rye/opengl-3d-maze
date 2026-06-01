c3527cf6f5b07ed5f26b62f950228cf8b6bf8d50
Ivan Li <ivanli10252006@gmail.com>
2026-06-01 19:20:37 +1000

    Add cube vertices into object rendering class

--- opengl-3d-maze/src/ObjectRenderer.cpp
+++ opengl-3d-maze/src/ObjectRenderer.cpp
@@ -3,7 +3,6 @@
 
 ObjectRenderer::ObjectRenderer(Shader& shader)
 {
-	
 }
 
 ObjectRenderer::~ObjectRenderer()
@@ -12,8 +11,50 @@ ObjectRenderer::~ObjectRenderer()
 
 void ObjectRenderer::initRenderData()
 {
-	float vertices[] =
-	{
-		0
-	};
+    float vertices[] = 
+    {
+        // pos coords          // tex coords
+        // Back face
+        -0.5f, -0.5f, -0.5f,   0.0f, 0.0f,
+         0.5f, -0.5f, -0.5f,   1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,   1.0f, 1.0f,
+         0.5f,  0.5f, -0.5f,   1.0f, 1.0f,
+        -0.5f,  0.5f, -0.5f,   0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,   0.0f, 0.0f,
+        // Front face
+        -0.5f, -0.5f,  0.5f,   0.0f, 0.0f,
+         0.5f, -0.5f,  0.5f,   1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,   1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,   1.0f, 1.0f,
+        -0.5f,  0.5f,  0.5f,   0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,   0.0f, 0.0f,
+        // Left face
+        -0.5f,  0.5f,  0.5f,   1.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,   1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,   0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,   0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,   0.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,   1.0f, 0.0f,
+        // Right face
+         0.5f,  0.5f,  0.5f,   1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,   1.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,   0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,   0.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,   0.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,   1.0f, 0.0f,
+         // Bottom face
+         -0.5f, -0.5f, -0.5f,   0.0f, 1.0f,
+          0.5f, -0.5f, -0.5f,   1.0f, 1.0f,
+          0.5f, -0.5f,  0.5f,   1.0f, 0.0f,
+          0.5f, -0.5f,  0.5f,   1.0f, 0.0f,
+         -0.5f, -0.5f,  0.5f,   0.0f, 0.0f,
+         -0.5f, -0.5f, -0.5f,   0.0f, 1.0f,
+         // Top face
+         -0.5f,  0.5f, -0.5f,   0.0f, 1.0f,
+          0.5f,  0.5f, -0.5f,   1.0f, 1.0f,
+          0.5f,  0.5f,  0.5f,   1.0f, 0.0f,
+          0.5f,  0.5f,  0.5f,   1.0f, 0.0f,
+         -0.5f,  0.5f,  0.5f,   0.0f, 0.0f,
+         -0.5f,  0.5f, -0.5f,   0.0f, 1.0f
+    };
 }
