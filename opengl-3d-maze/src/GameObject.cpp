89a6311bb0f7ed92429f27f1b128922fc7f7ec21
Ivan Li <ivanli10252006@gmail.com>
2026-06-03 17:09:55 +1000

    Create basic game object class to store object related variables

---
+++ opengl-3d-maze/src/GameObject.cpp
@@ -0,0 +1,34 @@
+#include "GameObject.h"
+
+GameObject::GameObject()
+{
+	Position = glm::vec3(0.0f);
+	Size = glm::vec3(1.0f);
+	Velocity = glm::vec3(0.0f);
+	Colour = glm::vec3(1.0f);
+	Rotation = glm::vec3(0.0f);
+
+	CanCollide = true;
+	Destroyed = false;
+
+	ObjectTexture = Texture();
+}
+
+GameObject::GameObject(Texture texture, glm::vec3 pos, glm::vec3 size, glm::vec3 colour, glm::vec3 velocity, glm::vec3 rotation)
+{
+	Position = pos;
+	Size = size;
+	Colour = colour;
+	Velocity = velocity;
+	Rotation = rotation;
+
+	CanCollide = true;
+	Destroyed = false;
+
+	ObjectTexture = texture;
+}
+
+void GameObject::Draw(ObjectRenderer& renderer)
+{
+	renderer.DrawObject(ObjectTexture, Position, Size, Rotation, Colour);
+}
---
+++ opengl-3d-maze/src/GameObject.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "ObjectRenderer.h"
+
+class GameObject
+{
+    glm::vec3 Position, Size, Velocity, Colour, Rotation;
+
+    bool CanCollide;
+    bool Destroyed;
+
+    Texture ObjectTexture;
+
+    GameObject();
+
+    GameObject(
+        Texture texture,
+        glm::vec3 pos, 
+        glm::vec3 size, 
+        glm::vec3 colour = glm::vec3(1.0f), 
+        glm::vec3 velocity = glm::vec3(0.0f),
+        glm::vec3 rotation = glm::vec3(0.0f)
+    );
+
+    virtual void Draw(ObjectRenderer& renderer);
+};
+
