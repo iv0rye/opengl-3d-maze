58390f28cdde00d7afd83e88b00453868bcdbcfd
Ivan Li <ivanli10252006@gmail.com>
2026-06-04 01:58:34 +1000

    Implement maze object representing a wall in the maze

--- opengl-3d-maze/src/MazeObject.cpp
+++ opengl-3d-maze/src/MazeObject.cpp
@@ -1,1 +1,16 @@
 #include "MazeObject.h"
+
+MazeObject::MazeObject(float xPos, float zPos, Texture texture)
+{
+	Position = glm::vec3(xPos, MAZE_WALL_HEIGHT, zPos);
+	Size = MAZE_WALL_SIZE;
+
+	Colour = glm::vec3(1.f);
+	Velocity = glm::vec3(0.f);
+	Rotation = glm::vec3(0.f);
+
+	CanCollide = true;
+	Destroyed = false;
+
+	ObjectTexture = texture;
+}
--- opengl-3d-maze/src/MazeObject.h
+++ opengl-3d-maze/src/MazeObject.h
@@ -3,6 +3,12 @@
 class MazeObject :
     public GameObject
 {
-    
+public:
+    MazeObject(float xPos, float yPos, Texture texture);
+
+private:
+    // maze wall constants
+    const float MAZE_WALL_HEIGHT = 1.0f;
+    const glm::vec3 MAZE_WALL_SIZE = glm::vec3(5.f, 6.f, 5.f);
 };
 
