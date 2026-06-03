d699fe7213df03f2b732e2503a50f591a950f8ac
Ivan Li <ivanli10252006@gmail.com>
2026-06-03 17:49:13 +1000

    Implement methods in camera class

--- opengl-3d-maze/src/Camera.cpp
+++ opengl-3d-maze/src/Camera.cpp
@@ -1,7 +1,9 @@
 #include "Camera.h"
 #include "Camera.h"
+#include <glm/trigonometric.hpp>
+#include <glm/ext/matrix_transform.hpp>
 
-Camera::Camera(int width, int height, float mouseSens)
+Camera::Camera(int width, int height, float mouseSens, float movementSens)
 {
 	Pos = glm::vec3(0.0f);
 	FrontDir = glm::vec3(0.0f, 0.0f, -1.0f);
@@ -14,6 +16,69 @@ Camera::Camera(int width, int height, float mouseSens)
 	Yaw = -90.0f;
 
 	MouseSensitivity = mouseSens;
+	MovementSensitivity = movementSens;
 
 	FirstMouse = true;
+}
+
+void Camera::ProcessKeyboard(GLFWwindow* window, float dt)
+{
+	const float cameraSpeed = MovementSensitivity * dt;
+
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+
+	// camera movement
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		Pos += FrontDir * cameraSpeed;
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		Pos -= FrontDir * cameraSpeed;
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+	// We get a right facing vector with cross product (ensure normalised)
+		Pos -= glm::normalize(glm::cross(FrontDir, UpDir)) * cameraSpeed;
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		Pos += glm::normalize(glm::cross(FrontDir, UpDir)) * cameraSpeed;
+}
+
+void Camera::ProcessMouse(GLFWwindow* window, double xpos, double ypos)
+{
+	// prevent mouse jumping with initial mouse read (sets offsets to 0 effectively)
+	if (FirstMouse)
+	{
+		LastX = xpos;
+		LastY = ypos;
+		FirstMouse = false;
+	}
+
+	// calculate offset of mouse movement in 2d window
+	float xOffset = xpos - LastX;
+	float yOffset = LastY - ypos;
+	LastX = xpos;
+	LastY = ypos;
+
+	// multiply offset by a sensitivity value
+	const float sensitivity = 0.1f;
+	xOffset *= sensitivity;
+	yOffset *= sensitivity;
+
+	// add offset to yaw/pitch angles
+	Yaw += xOffset;
+	Pitch += yOffset;
+
+	// clamp
+	Pitch = std::max(-89.0f, std::min(Pitch, 89.0f));
+
+	// calculate direction
+	glm::vec3 direction;
+
+	direction.x = cos(glm::radians(Yaw)) * cos(glm::radians(Pitch));
+	direction.y = sin(glm::radians(Pitch));
+	direction.z = sin(glm::radians(Yaw)) * cos(glm::radians(Pitch));
+
+	FrontDir = glm::normalize(direction);
+}
+
+glm::mat4 Camera::GetViewMatrix()
+{
+	return glm::lookAt(Pos, Pos + FrontDir, UpDir);
 }
--- opengl-3d-maze/src/Camera.h
+++ opengl-3d-maze/src/Camera.h
@@ -2,6 +2,7 @@
 
 #include <glm/ext/vector_float3.hpp>
 #include <glm/ext/matrix_float4x4.hpp>
+#include <GLFW/glfw3.h>
 
 class Camera
 {
@@ -14,10 +15,17 @@ public:
 	// mouse movement related
 	float LastX, LastY, Pitch, Yaw;
 
-	float CameraSpeed, MouseSensitivity;
+	float MouseSensitivity;
+	float MovementSensitivity;
 
 	bool FirstMouse;
 
-	Camera(int width = 800, int height = 400, float mouseSens = 0.1f);
+	Camera(int width = 800, int height = 400, float mouseSens = 0.1f, float movementSens = 3.5f);
+
+	void ProcessKeyboard(GLFWwindow* window, float dt);
+
+	void ProcessMouse(GLFWwindow* window, double xpos, double ypos);
+
+	glm::mat4 GetViewMatrix();
 };
 
